c3d58800c75fd2507cd139233ed4c1e5623ee2db
hbsbashitha <hbsbashitha@gmail.com>
2021-08-06 18:09:29 +0530

    complete 5.6

--- first_react_app/first-app/src/App.js
+++ first_react_app/first-app/src/App.js
@@ -1,58 +1,125 @@
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 import React,{Component} from 'react';
-import './App.css'
-import Validation from './Validation/Validation';
-import Char from './Char/Char'
+import './App.css';
+import Person from './Person/Person';
+import Radium ,{StyleRoot} from 'radium';
 
-class App extends Component{
+class App extends Component {
 
-  state={
-    userInput:''
-  }
+  state=({
+      Person:[{ id:'bh1',Name:'Max',Age:'28'},
+      {id:'bh2',Name:'Manu',Age:'29'},
+      {id:'bh3',Name:'Stephanie',Age:'26'}],
 
+      Status:false
 
-  inputChangeHandler=(event) =>{
-
-    this.setState(
-      {userInput:event.target.value}
-    )
-  }
-
-
-  deleteCharacter = (index) =>{
-    const text=this.state.userInput.split('');
-    text.splice(index,1);
-    const updatedText=text.join('');
-    this.setState({userInput:updatedText});
-  }
-  render(){
-
-    const charList=this.state.userInput.split("").map((ch,index)=>{
-      return (<Char character={ch} key={index} delete ={()=>this.deleteCharacter(index)}/>)
     })
 
-    return (
-      <div className="App">
-        <p>{this.state.userInput}</p>
-        <input type='text' onChange={this.inputChangeHandler} value={this.state.userInput}/>
-        <Validation length={this.state.userInput.length}/>
-        {charList}
-      </div>
-    )
+    deletePerson = (index) => {
+      // const persons = this.state.Person.slice();
+      const persons = [...this.state.Person]
+      persons.splice(index,1);
+      this.setState({Person: persons});
+
+    }
+
+  switchToUsers= () => {
+   
+    const tempStatus=this.state.Status;
+    this.setState({Status:!tempStatus});
+    
+  };
+  eventHandler=(event,id)=> {
+    
+    const personIndex=this.state.Person.findIndex(p=>{
+      return p.id===id;
+
+    });
+
+    const per={...this.state.Person[personIndex]};
+ 
+    per.Name=event.target.value;
+   
+
+    const pers=[...this.state.Person];
+    pers[personIndex]=per;
+    
+
+    this.setState(
+      {
+        // Person:[{ id:'bh1',Name:event.target.value,Age:'28'},
+        // {id:'bh2',Name:'Manu',Age:'29'},
+        // {id:'bh3',Name:'Stephanie',Age:'26'}],
+
+        Person:pers
+  
+      }
+    );
   }
 
+ 
+  
+
+render(){
+  const styles={
+    background:'green',
+    color:'white',
+    font:'inherit',
+    border:'1px solid blue',
+    padding:'8px',
+    cursor:'pointer',
+    ':hover':{
+      background:'lightgreen',
+      color:'black'
+    }
+  };
+
+
+  let person=null;
+
+  if (this.state.Status) {
+    person=(
+      <div>
+        {this.state.Person.map((person,index)=>{
+          return (<Person name={person.Name}
+             age={person.Age}
+              delete={()=>this.deletePerson(index)} 
+              key={person.id} 
+              changed={(event) => this.eventHandler(event,person.id)}/>)
+        })}
+      </div> 
+    )
+
+    styles.background='red';
+    styles[':hover']={
+      background:'salmon',
+      color:'black'
+      }
+    }
+
+
+      let classes=[];
+
+      if(this.state.Person.length<=2){
+        classes.push('red');
+      }
+      if(this.state.Person.length<=1){
+        classes.push('bold');
+      }
+  return(
+
+<StyleRoot>
+<div className="App">
+      
+      <h1>Hi,I'm a React App</h1>
+      <p className={classes.join(' ')}>This is really working!</p>
+      <button style={styles} onClick={this.switchToUsers}>Switch Name</button>
+      {person}
+    </div>
+</StyleRoot>
+  )
+  }
 }
-export default App;
+
+
+export default Radium(App);
